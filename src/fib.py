797e2674b60d99ff686bd5b99303b592d420bd4b
Linuxea@foxmail.com <Linuxea@foxmail.com>
2017-12-26 18:49:21 +0800

    菲波那契函数

---
+++ src/fib.py
@@ -0,0 +1,21 @@
+
+""" 菲波那契函数 
+"""
+
+class Fib:
+    def __init__(self):
+        print("init")
+        self.a, self.b = 1, 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.a, self.b = self.b, self.a + self.b
+        if self.a > 100:
+            raise StopIteration
+        return self.a
+
+
+for n in Fib():
+    print(n)
