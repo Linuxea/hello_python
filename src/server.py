fff55b73e19b707331e6c4dfb2c5773ed624e92b
Linuxea@foxmail.com <Linuxea@foxmail.com>
2018-01-09 13:21:21 +0800

    why

--- src/server.py
+++ src/server.py
@@ -5,30 +5,38 @@ from socket import *
 import time
 
 HOST = '127.0.0.1'
-PORT = 9090
+PORT = 21567
 BUFSIZE = 1024
 
 server_socket = socket(AF_INET, SOCK_STREAM)
 server_socket.bind((HOST, PORT))
-server_socket.listen(5)
+server_socket.listen(90)
 
 while True:
 
     try:
 
+        print("server socket blocking...")
+
         client_socket, addr = server_socket.accept()
+
+        print("client_socket: %s" % (client_socket))
 
         while True:
             try:
                 data = client_socket.recv(BUFSIZE)
+                print("%s data:" %(data))
                 if not data:
                     break
-                re = bytes("%si have receive:%s" % (time.ctime(), data), encoding = "utf8") 
+                # re = bytes("%si have receive:%s" % (time.ctime(), data), encoding = "utf8") 
+                re = bytes('ok', encoding = "utf8")
+                print("返回消息预览:%s" %(re))
                 client_socket.send(re)
             except Exception as e:
                 print("%s" % e)
+                time.sleep(10)
+            finally:
                 client_socket.close()
-                time.sleep(5)
 
     except Exception as e :
         print("%s" % e)
