0cc66fe1231aaa7188430352beaacd5f2a87c9d9
Linuxea@foxmail.com <Linuxea@foxmail.com>
2018-01-18 22:50:42 +0800

    oop

---
+++ src/oo/Student.py
@@ -0,0 +1,19 @@
+class Student(object):
+
+    ''' 和普通的函数相比，在类中定义的函数只有一点不同，就是第一个参数永远是实例变量self '''
+
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    
+    def get_name(self):
+        print("name: %s" % self.name)
+
+    def get_score(self):
+        print("score: %s" % self.score)
+
+
+stu = Student('linuxea', 199)
+stu.get_name()
+stu.get_score()
---
+++ src/oo/private_field.py
@@ -0,0 +1,25 @@
+
+
+__author__ = 'linuxea'
+__doc__ = '__私有变量  __特殊变量__  _虽然我可以被访问但是请把我视为私有变量，不要随意访问'
+
+class Me():
+
+    def __init__(self, name, age):
+        self.name = name
+        self.__age = age
+
+    def get_name(self):
+        return self.name
+
+    def get_age(self):
+        return self.__age
+
+
+
+me = Me('linuxea', 29)
+
+print("%s" % me.get_name())
+print("%s" % me.get_age())
+''' we cant '''
+# print("%s" % me__age) 
