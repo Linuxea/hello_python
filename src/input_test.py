328a3f40d23f5edf2da9c6f4c3da7197f8ef2089
Linuxea@foxmail.com <Linuxea@foxmail.com>
2018-01-19 15:22:27 +0800

    input in p2&p3

---
+++ src/input_test.py
@@ -0,0 +1,9 @@
+#coding = utf-8
+
+
+''' python3 默认将输入的东东看作是字符串
+    python2 需要使用 raw_input() 将输入的东东转化为字符串，默认保留输入东东的原本数据类型 
+'''
+something = input("请输入一些东东:\t")
+
+print("something you input is: %s" % something)
