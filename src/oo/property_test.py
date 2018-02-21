1e4f9a113ca3ba6eb403a0a24a0771fa18fba6ff
Linuxea@foxmail.com <Linuxea@foxmail.com>
2018-02-21 22:37:11 +0800

    test

---
+++ src/oo/property_test.py
@@ -0,0 +1,27 @@
+class Student(object):
+
+
+    @property
+    def score(self):
+        return self._score
+    
+    @score.setter
+    def set_score(self, value):
+        if not isinstance(value, (int, float)):
+            raise ValueError('分数需要为数字')
+        self._score = value
+
+s = Student()
+
+
+# old way
+# s.set_score(123)
+# print(s.score)
+# s.set_score('nothing')
+# print(s.score)
+
+
+# new way
+# why error
+s.score = 123
+print(s.score)
---
+++ src/oo/slots_test.py
@@ -0,0 +1,23 @@
+class Student:
+
+    __slots__ = ('__name__', '__age__') # 用tuple定义允许绑定的属性名称
+
+    def __init__(self, name, age):
+        self.__name__ = name
+        self.__age__ = age
+
+    def print_me(self):
+        print("%s's age is %s" % (self.__name__, self.__age__))
+
+
+    
+
+s = Student("linuxea", 90)
+s.print_me()
+print("dir s:%s" % dir(s))
+print("s is:%s" % s)
+
+# error because of __slots__
+# s.ok = 12
+# print("s.ok is:%s" % s.ok)
+
