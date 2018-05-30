b29da8add38907e817b649debd2b8620890f5085
Linuxea@foxmail.com <linuxea@foxmail.com>
2018-05-31 01:12:40 +0800

    session

--- src/components/requests/req.py
+++ src/components/requests/req.py
@@ -2,5 +2,22 @@
 
 import requests
 
-r = requests.get("http://test.api.user.huluxia.com/contacts/2.1?apk_id=102")
+se = requests.session()
+url = "http://test.api.user.huluxia.com/contacts/2.1?apk_id=102"
+r = se.get(url)
 print(r.json())
+
+print(r.headers)
+print(r.text)
+print(r.cookies)
+print(r.encoding)
+print(r.status_code)
+print(r.url)
+print(r.ok)
+
+print(type(r.cookies))
+
+for x, y in r.cookies:
+    print("%s -> %s" % (x, y))
+
+requests.get("", header={})
